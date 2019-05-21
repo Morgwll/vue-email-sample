0f0c7a71f8967aa2558c0d3c4b7ffe315548df69
Joseph Ortega <joseph.ortega.design@gmail.com>
2019-05-21 11:03:47 +0100

    Story generator working

--- src/components/environment/Loaded.vue
+++ src/components/environment/Loaded.vue
@@ -19,19 +19,27 @@
                 </ul>
             </div>
         </div>
-        <div v-if="shown !== ''">
-            <img :src="portraitChooser(shown)">
-            {{ shown.characterType }}
+        <div v-if="shown !== ''" class="loadedDisplayPanel">
+            <h2>{{ shown.characterClass }}</h2>
+            <div class="loadedDisplayContainer">
+                <img :src="portraitChooser(shown)">
+                <div class="description" v-html="shown.description"></div>
+            </div>
         </div>
-        <div v-else-if="shown === ''">
+        <div v-else-if="shown === ''" class="loadedDisplayPanel">
             <ul>
-                <li v-for="(storyElement, index) of $store.state.storyElements" :key="index">{{ storyElement.type }}</li>
+                <li v-for="(storyElement, index) of $store.state.storyElements" :key="index">
+                    <h3>{{ storyElement }}</h3>
+                </li>
             </ul>
         </div>
         <div class="buttonContainer"><router-link tag="button" to="/encounter">Go to Encounter</router-link></div>
     </div>
 </template>
 <script>
+    import locations from '../../data/locations.json';
+    import plots from '../../data/plots.json';
+    import mixins from '../../mixins.js';
     export default {
         data() {
             let chosenWorld = 0;
@@ -46,6 +54,7 @@
             const pantheon = this.$store.state.worlds[chosenWorld].environment.pantheon;
             let menus = this.$store.state.worlds[chosenWorld].environment.plots;
             let shown = "";
+            let story = '';
             const content = {
                 "title": this.$store.state.selectedGame.name,
                 "lastDescription": this.$store.state.selectedGame.synopsis
@@ -60,8 +69,33 @@
                 shown,
                 content,
                 portents,
+                story,
                 locations,
-                chosenWorld
+                chosenWorld,
+                globalTime: {
+                    months: {
+                        january: "January",
+                        february: "February",
+                        march: "March",
+                        april: "April",
+                        may: "May",
+                        june: "June",
+                        august: "August",
+                        september: "September",
+                        october: "October",
+                        november: "November",
+                        december: "December"
+                    },
+                    days: {
+                        monday: "Monday",
+                        tuesday: "Tuesday",
+                        wednesday: "Wednesday",
+                        thursday: "Thursday",
+                        friday: "Friday",
+                        saturday: "Saturday",
+                        sunday: "Sunday"
+                    }
+                }
             }
         },
         methods: {
@@ -100,11 +134,109 @@
                 this.hideLeftMenu();
             },
             addElement(element) {
-                this.$store.state.storyElements.push(element);
+                this.plotGenerator(element);
+                this.$store.state.storyElements.push(this.story);
             },
             showElement(element) {
                 this.shown = element;
-            }
+            },
+            chooseOption(options) {
+                const optionsNumber = Math.floor(Math.random() * Object.keys(options).length);
+                const choosy = Object.values(options);
+                for(let item in choosy) {
+                return choosy[optionsNumber];
+                }
+            },
+            booleanGen() {
+                const extNum = Math.floor(Math.random() * 2);
+                return extNum;
+            },
+            chooseNPC() {
+                const num = this.booleanGen();
+                const name = this.generateName(num);
+                const npcAlignment = this.chooseOption(plots.NPC.alignment);
+                const npcBackground = this.chooseOption(plots.NPC.background);
+                const npcLeitmotiv = this.chooseOption(plots.NPC.leitmotiv);
+                return name + " (who is " + npcAlignment + "), " + npcBackground + " and " + npcLeitmotiv;
+            },
+            timeFrame() {
+                const timeframe = this.chooseOption(plots.timeframe);
+                const yesNo = this.booleanGen();
+                if(yesNo == 0) {
+                return timeframe + ". ";
+                } else {
+                return "";
+                }
+            },
+            externalPlot(option) {
+                const choosy = Object.values(plots.externalPlot);
+                for(let item in choosy) {
+                const externalPlot = this.chooseOption(choosy[option]);
+                const yesNo = this.booleanGen();
+                if(yesNo == 0) {
+                    return "Also, " + externalPlot + " is affecting the region.";
+                } else {
+                    return "";
+                }
+                }
+            },
+            plotTwist() {
+                const plotTwist = this.chooseOption(plots.plotTwist);
+                const yesNo = this.booleanGen();
+                if(yesNo == 0) {
+                return  " but unbeknownst to the heroes, " + plotTwist;
+                } else {
+                return "";
+                }
+            },
+            religiousPlot() {
+                const religiousPlot = this.chooseOption(plots.religionPlot);
+                const yesNo = this.booleanGen();
+                if(yesNo == 0) {
+                return "Also, " + religiousPlot;
+                } else {
+                return "";
+                }
+            },
+            getMonth(number) {
+                const months = Object.values(this.globalTime.months);
+                const month = months[number];
+                return month;
+            },
+            getDay(number) {
+                const days =  Object.values(this.globalTime.days);
+                const day = days[number];
+                return day;
+            },
+            getSeason(month) {   
+                if ((month == 2) || (month == 3) || (month == 4)) {
+                return 'Spring';
+                } else if ((month == 5) || (month == 6) || (month == 7)) {
+                return 'Summer';
+                } else if ((month == 8) || (month == 9) || (month == 10)) {
+                return 'Autumn';
+                } else {
+                return 'Winter';
+                }
+            },
+            plotGenerator(env) {
+                console.log("the display ", env);
+                //const title = env.characterClass;
+                //const portrait = env.portrait;
+                const extNum = this.booleanGen();
+                const monthNum = Math.floor(Math.random() * 12);
+                const dayNum = Math.floor(Math.random() * 7);
+                const yearNum = "Y" + (3250 + Math.floor(Math.random() * 15));
+                const day = this.getDay(dayNum);
+                const month = this.getMonth(monthNum);
+                const season = this.getSeason(monthNum);
+                const npc = this.chooseNPC();
+                const time = this.timeFrame();
+                const extPlot = this.externalPlot(extNum);
+                const plotTwist = this.plotTwist();
+                const religiousPlot = this.religiousPlot();
+                this.story = "It is the " + season + " of " + yearNum + ". The date, " + day + " the " + (dayNum + 1) + " of " + month + ". " + npc + ". " + time + extPlot + religiousPlot + plotTwist;
+            },
         }
     }
 </script>
